62e2005d5b580f1c3b7bc6abba09959952f4d17c
Hplatapus2 <84129139+Hplatapus2@users.noreply.github.com>
2023-07-25 18:46:09 +1000

    Delete centerdown.php

--- centerdown.php
+++
@@ -1,5 +0,0 @@
-root@test:/var/www/html# cat testscript/testexec.php
-<?php
-shell_exec("/home/hudsonfoster25/Documents/flaps.py/cmdprmt_servo/centerdown");
-header('Location: http://192.168.1.222/testscript/index.html?success=true');
-?>
